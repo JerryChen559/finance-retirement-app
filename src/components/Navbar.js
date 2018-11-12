117970421de8c881bd7b68de69b1b48221c0f61f
Jerry <kickassmusic4vr@hotmail.com>
2018-11-11 23:33:44 -0600

    navbar dark on scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,27 @@ class Navbar extends Component {
     this.state = {
       // loggedIn: false,
       // user: {}
+
+      // toggle on Scroll
+      isTop: true
     };
+    this.onScroll = this.onScroll.bind(this);
   }
 
   componentDidMount() {
     this.props.getUser();
+
+    // toggle on Scroll
+    document.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+        this.onScroll(isTop);
+      }
+    });
+  }
+
+  onScroll(isTop) {
+    this.setState({ isTop });
   }
 
   render() {
@@ -28,6 +44,21 @@ class Navbar extends Component {
     const redirectLogout = () => {
       window.location.href = `${process.env.REACT_APP_SERVER}/logout`;
     };
+
+    // Navbar background turns dark on scroll
+    // plain js
+    // I'm using the react way in this app.
+    // var myNav = document.getElementById("mynav");
+    // window.onscroll = function() {
+    //   // "use strict";
+    //   if (document.body.scrollTop >= 200) {
+    //     myNav.classList.add("nav-colored");
+    //     myNav.classList.remove("nav-transparent");
+    //   } else {
+    //     myNav.classList.add("nav-transparent");
+    //     myNav.classList.remove("nav-colored");
+    //   }
+    // };
 
     // use this to conditionally render navbar based on loggedin state
     let appname = this.props.profile.loggedIn ? (
@@ -46,25 +77,29 @@ class Navbar extends Component {
       </div>
     );
 
+    // use this to conditionally render navbar
+    // background color based on isTop state
+    let scrolltoggle = this.state.isTop
+      ? "nav-wrapper-transparent"
+      : "nav-wrapper-dark";
+
     return (
-      // <div className="navbar-bg">
-      // still need to make the nav bar background turn grey on scroll
+      // <div style={{ height: "200vh" }}>
+      <div>
+        <div className={scrolltoggle}>
+          <div className="navbar-name">{appname}</div>
 
-      <div className="nav-wrapper">
-        <div className="navbar-name">{appname}</div>
+          <ul className="nav-items">
+            <li className="has-divider learnmore">
+              <Link to="/learnmore">
+                <div>Learn More </div>
+              </Link>
+            </li>
 
-        <ul className="nav-items">
-          <li className="has-divider learnmore">
-            <Link to="/learnmore">
-              <div>Learn More </div>
-            </Link>
-          </li>
-
-          <li className="has-divider signup">{logtoggle}</li>
-        </ul>
+            <li className="has-divider signup">{logtoggle}</li>
+          </ul>
+        </div>
       </div>
-
-      // </div>
     );
   }
 }
